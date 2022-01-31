d441dd79c986c4494873427dcd2bf355f365845b
Giritharan Balasubramaniam <giri@Giritharans-MacBook-Pro.local>
2022-01-30 20:47:51 -0600

    combine Action stream vs Data Stream

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,7 +1,8 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
-import { EMPTY, Subscription } from 'rxjs';
-import { catchError } from 'rxjs/operators';
+import { BehaviorSubject, combineLatest, EMPTY, Subject, Subscription } from 'rxjs';
+import { catchError, map, startWith } from 'rxjs/operators';
+import { ProductCategoryService } from '../product-categories/product-category.service';
 
 import { Product } from './product';
 import { ProductService } from './product.service';
@@ -14,23 +15,35 @@ import { ProductService } from './product.service';
 export class ProductListComponent  {
   pageTitle = 'Product List';
   errorMessage = '';
-  categories;
+  //selectedCategoryId = undefined;
 
-  products$  = this.productService.productWithCategory$
-                                  .pipe(catchError(err => {
-                                    this.errorMessage = err;
-                                    return EMPTY;
-                                  }))
+  private selectedCategorySubject = new Subject<number>();
+  selectedCategoryAction$ = this.selectedCategorySubject.asObservable().pipe(startWith(0));
+
+  categories$ = this.productCategories.productCategories$;
+  products$  = combineLatest([this.productService.productWithCategory$, 
+                              this.selectedCategoryAction$])
+                          .pipe(map(([products , categoryId]) =>
+                                    products.filter(e => categoryId ? e.categoryId == categoryId : true)
+                                  
+                          ));
+
+
+ // productsFilter$ = this.productService.productWithCategory$
+                                     // .pipe(map(products=> products.filter(e => this.selectedCategoryId ? e.categoryId === this.selectedCategoryId : true)))
   sub: Subscription;
 
-  constructor(private productService: ProductService) { }
+  constructor(private productService: ProductService,
+    private productCategories : ProductCategoryService) { }
 
 
   onAdd(): void {
     console.log('Not yet implemented');
   }
 
-  onSelected(categoryId: string): void {
-    console.log('Not yet implemented');
+  onSelected(categoryId: number): void {
+    console.log('Not yet implemented', categoryId);
+
+    this.selectedCategorySubject.next(categoryId);
   }
 }
